8af96520e9d914a9122203f6990265e1d0d5a8f7
Sitam <sitammeur@gmail.com>
2023-09-12 00:50:01 +0530

    login fields changed

--- forum/views.py
+++ forum/views.py
@@ -16,16 +16,16 @@ def loginpageview(request):
         return redirect("homepageview")
 
     if request.method == "POST":
-        email = request.POST.get("email").lower()
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(username=username)
 
         except Exception:
             messages.info(request, "User does not exist!")
 
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect("homepageview")
