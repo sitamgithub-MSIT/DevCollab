889019b490e8268cf2e3cbab7076c55b522193de
Sitam <sitammeur@gmail.com>
2023-09-11 22:50:54 +0530

    secret key changes

--- devcollab/settings.py
+++ devcollab/settings.py
@@ -25,7 +25,8 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY='django-insecure-1!)$ntdki=&v)#pbx#lcswpa@hf224n(l-63snxw!q3&i2@e@-'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
