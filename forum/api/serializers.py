14a6ec7ba31a7e57cb4e6ffae0763740dd15dc22
Sitam <sitammeur@gmail.com>
2024-02-05 01:45:12 +0530

    formatting and docs added to api

--- forum/api/serializers.py
+++ forum/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from forum.models import Forum
 
+
 class ForumSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Forum model.
+    """
+
     class Meta:
         model = Forum
-        fields = '__all__'
+        fields = "__all__"
--- forum/api/urls.py
+++ forum/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
 
+"""
+This module defines the URL patterns for the forum API.
+
+- The path '' maps to the getroutes view function.
+- The path 'rooms/' maps to the getrooms view function.
+- The path 'rooms/<str:pk>/' maps to the getroom view function.
+"""
 
 urlpatterns = [
-
-    path('' , views.getroutes),
-    path('rooms/', views.getrooms),
-    path('rooms/<str:pk>/', views.getroom),
-
+    path("", views.getroutes),
+    path("rooms/", views.getrooms),
+    path("rooms/<str:pk>/", views.getroom),
 ]
