ee10c98f701eb9492fde60dfaaf624c5ce1b778a
Sitam <sitammeur@gmail.com>
2024-02-05 02:37:01 +0530

    docs added app urls

--- forum/urls.py
+++ forum/urls.py
@@ -1,22 +1,24 @@
 from django.urls import path
 from forum import views
 
+"""
+URL patterns for the forum application.
+
+This list defines the mapping between URL patterns and corresponding view functions.
+"""
 
 urlpatterns = [
-    path('' , views.homepageview, name='homepageview'),
-    path('topic/<str:pk>/', views.topicpageview, name='topicpageview'),
-    path('profile/<str:pk>/', views.profilepageview, name='profilepageview'),
-
-    path('create-topic/', views.createtopicview, name='createtopicview'),
-    path('update-topic/<str:pk>/', views.updatetopicview, name='updatetopicview'),
-    path('delete-topic/<str:pk>/', views.deletetopicview, name='deletetopicview'),   
-    path('delete-message/<str:pk>/', views.deletemessageview, name='deletemessageview'),
-    path('update-user/', views.updateuserview, name='updateuserview'),
-    path('category/', views.categorypageview, name='categorypageview'),
-    path('activity/', views.activitypageview, name='activitypageview'),
-
-
-    path('login/', views.loginpageview, name='loginpageview'),
-    path('logout/', views.logoutpageview, name='logoutpageview'),
-    path('register/', views.registerpageview, name='registerpageview'),
+    path("", views.homepageview, name="homepageview"),
+    path("topic/<str:pk>/", views.topicpageview, name="topicpageview"),
+    path("profile/<str:pk>/", views.profilepageview, name="profilepageview"),
+    path("create-topic/", views.createtopicview, name="createtopicview"),
+    path("update-topic/<str:pk>/", views.updatetopicview, name="updatetopicview"),
+    path("delete-topic/<str:pk>/", views.deletetopicview, name="deletetopicview"),
+    path("delete-message/<str:pk>/", views.deletemessageview, name="deletemessageview"),
+    path("update-user/", views.updateuserview, name="updateuserview"),
+    path("category/", views.categorypageview, name="categorypageview"),
+    path("activity/", views.activitypageview, name="activitypageview"),
+    path("login/", views.loginpageview, name="loginpageview"),
+    path("logout/", views.logoutpageview, name="logoutpageview"),
+    path("register/", views.registerpageview, name="registerpageview"),
 ]
